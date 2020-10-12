a897551d9247f76b199386a6acc43fcd2b6cdfd6
milkdudss <albertkoshy@gmail.com>
2020-10-11 22:05:07 -0700

    f

---
+++ week3assignment1/exercise1.js
@@ -0,0 +1,6 @@
+const firstName = prompt(`what is your first name?`)
+const lastName = prompt(`what is your last name?`)
+const age = prompt(`what is your age?`)
+
+console.log(`Your full name is ${firstName} ${lastName}.`)
+console.log(`You are ${age} years old.`)
